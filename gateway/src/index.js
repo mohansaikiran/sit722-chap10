0514f32f2ea60aed2e7ced71d4b6ec60b5eeba65
Mohan Bonu <kiranms20@gmail.com>
2024-10-04 23:45:24 +1000

    config-changes

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -9,7 +9,7 @@ if (!process.env.PORT) {
 const PORT = process.env.PORT;
 
 //
-// Application entry point.
+// Application entry point. comments
 //
 async function main() {
     const app = express();
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 const RABBIT = process.env.RABBIT;
 
 //
-// Application entry point.
+// Application entry point. comments
 //
 async function main() {
     const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
